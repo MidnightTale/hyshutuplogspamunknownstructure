f19b73df1c6e2dd0056be0992ced52900215c79f
MidnightTale <midnighttale24@gmail.com>
2023-08-28 03:31:08 +0700

    huh

--- src/main/java/xyz/hynse/hyshutuplogspamunknownstructure/Hyshutuplogspamunknownstructure.java
+++ src/main/java/xyz/hynse/hyshutuplogspamunknownstructure/Hyshutuplogspamunknownstructure.java
@@ -2,55 +2,86 @@ package xyz.hynse.hyshutuplogspamunknownstructure;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerCommandEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public final class Hyshutuplogspamunknownstructure extends JavaPlugin {
-    private final Map<String, Integer> logCounts = new HashMap<>();
-    private final Map<String, Long> logTimestamps = new HashMap<>();
-    private final long aggregationInterval = 2 * 1000;
+public final class Hyshutuplogspamunknownstructure extends JavaPlugin implements Listener {
+    private final LogAggregator logAggregator = new LogAggregator();
 
     @Override
     public void onEnable() {
-        // Register the event listener
-        getServer().getPluginManager().registerEvents(new LogFilterListener(), this);
+        getServer().getPluginManager().registerEvents(this, this); // Register the outer class as a listener
     }
 
-    private class LogFilterListener implements org.bukkit.event.Listener {
+    private class LogAggregator implements Runnable {
+        private final Map<String, Integer> logCounts = new HashMap<>();
+        private final Map<String, Long> logTimestamps = new HashMap<>();
+        private final long aggregationInterval = 3000; // 3 seconds
+        private boolean aggregating = false;
+
         @EventHandler
         public void onServerCommand(ServerCommandEvent event) {
             String logMessage = event.getCommand();
+            String pattern = getLogPattern(logMessage);
 
-            // Define the patterns for different log messages you want to handle
+            if (pattern != null) {
+                logCounts.put(pattern, logCounts.getOrDefault(pattern, 0) + 1);
+                logTimestamps.put(pattern, System.currentTimeMillis());
+
+                if (!aggregating) {
+                    FoliaScheduler.runGlobalDelay(Hyshutuplogspamunknownstructure.this, this, aggregationInterval);
+                    aggregating = true;
+                }
+            }
+        }
+
+        @Override
+        public void run() {
+            long currentTime = System.currentTimeMillis();
+
+            while (currentTime - logTimestamps.values().stream().min(Long::compare).orElse(currentTime) < aggregationInterval) {
+                try {
+                    Thread.sleep(aggregationInterval);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                currentTime = System.currentTimeMillis();
+            }
+
+            for (Map.Entry<String, Integer> entry : logCounts.entrySet()) {
+                String pattern = entry.getKey();
+                int count = entry.getValue();
+                long timestamp = logTimestamps.getOrDefault(pattern, 0L);
+
+                if ((currentTime - timestamp) >= aggregationInterval) {
+                    Bukkit.getLogger().info(pattern + " (x" + count + ") in " + (currentTime - timestamp) + " ms");
+                    logCounts.remove(pattern);
+                    logTimestamps.remove(pattern);
+                }
+            }
+
+            aggregating = false;
+        }
+
+        private String getLogPattern(String logMessage) {
             String[] patterns = {
                     "Unknown structure start",
                     "Found reference to unknown structure"
+                    // Add more patterns here as needed
             };
 
             for (String pattern : patterns) {
                 if (logMessage.contains(pattern)) {
-                    logCounts.put(pattern, logCounts.getOrDefault(pattern, 0) + 1);
-                    logTimestamps.put(pattern, System.currentTimeMillis());
-
-                    // Schedule a task to check and display stacked messages after the aggregation interval
-                    FoliaScheduler.runGlobalDelay(Hyshutuplogspamunknownstructure.this, () -> {
-                        int count = logCounts.getOrDefault(pattern, 0);
-                        long timestamp = logTimestamps.getOrDefault(pattern, 0L);
-                        long currentTime = System.currentTimeMillis();
-
-                        if (count > 0 && (currentTime - timestamp) < aggregationInterval) {
-                            Bukkit.getLogger().info(pattern + " (x" + count + ") in " + (currentTime - timestamp) + " ms");
-                            logCounts.remove(pattern);
-                            logTimestamps.remove(pattern);
-                        }
-                    }, aggregationInterval / 50); // Convert interval to server ticks
-
-                    break; // No need to check other patterns
+                    return pattern;
                 }
             }
+
+            return null;
         }
     }
 }
